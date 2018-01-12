ea36ef6d3ca261d1b895c971ca82f9bade236d0b
Noah Stahl <n.stahl@me.com>
2018-01-12 16:26:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,6 @@
 
 board = ["", "", "", "", "", "", "", "", "", ]
 
-def display_board(rows)
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
 end
