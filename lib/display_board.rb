aa5b34d74b6ef5c50fe000d426379b30d63de6cd
Noah Stahl <n.stahl@me.com>
2018-01-12 16:17:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,4 @@
 # out the current state.
 def display_board(board = ["X", "O", "O", "X", "X", "X", "O", "X", "X"])
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  /n----------/n #{board[3]}  | #{board[4]}  | #{board[5]}  /n----------/n #{board[6]}  | #{board[7]}  | #{board[8]}  "
+end
