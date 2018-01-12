3c4492c3cf90d0866ee77dcdd0451b187e7812ee
Noah Stahl <n.stahl@me.com>
2018-01-12 16:22:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(rows = ["   |   |   "])
+
+rows = ["   |   |   "]
+
+def display_board(rows)
   puts "#{rows[0]}"
 end
